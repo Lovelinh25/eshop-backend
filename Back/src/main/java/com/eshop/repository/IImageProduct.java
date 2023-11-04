049d392460a35b340fd90b4828c6ecfb3561945a
dang doan dong <doandong2k2@gmail.com>
2023-11-04 19:45:20 +0700

    fist

--- Back/src/main/java/com/eshop/repository/IImageProduct.java
+++
@@ -1,7 +0,0 @@
-package com.eshop.repository;
-
-import com.eshop.entity.ImageProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IImageProduct extends JpaRepository<ImageProduct, Integer> {
-}
